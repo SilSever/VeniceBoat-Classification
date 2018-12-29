a583934098fe45206e7abb259b25f029e3308c7d
Silvio Severino <silvio.severino811@gmail.com>
2018-12-29 23:12:57 +0100

    Add files via upload

---
+++ dataset_manipulation.py
@@ -0,0 +1,135 @@
+import shutil
+import os
+import cv2
+from imutils import paths
+import random
+
+import tensorflow as tf
+from tensorflow.python.keras.preprocessing.image import img_to_array
+
+
+
+def split_test_into_subfolders(path):
+    
+    dest = 'sc5-Test-tensorflow'
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+    
+    with open(path) as file:
+        
+        for line in file:
+            file_name, class_name = line.split(';')
+            class_name = class_name.replace('\n','')
+            
+            subdest = dest + '/' + class_name
+            if not os.path.exists(subdest):
+                os.makedirs(subdest)
+            shutil.copy('sc5-Test/'+file_name, subdest)
+            
+        file.close()
+
+
+def split_test_into_family_class(path):
+    
+    dest = 'sc5-test-tensorflow'
+    start = 'sc5-Test-tensorflow-tmp'
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+        
+    with open(path) as file:
+        
+        for line in file:
+            file_name, class_name = line.split(';')
+            class_name = class_name.replace('\n','')
+            
+            if os.path.exists(start+'/'+file_name):
+                subdest = dest + '/' + class_name
+                
+                if not os.path.exists(subdest):
+                    os.makedirs(subdest)
+                shutil.move(start+'/'+file_name,subdest)
+        file.close()  
+
+
+
+def split_into_family_class(path):
+    
+    dest = 'sc5-tensorflow'
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+        
+    with open(path) as file:
+        
+        for line in file:
+            file_name, class_name = line.split(';')
+            class_name = class_name.replace('\n','')
+            
+            if os.path.exists('sc5/'+file_name):
+                subdest = dest + '/' + class_name
+                
+                if not os.path.exists(subdest):
+                    os.makedirs(subdest)
+                shutil.move('sc5/'+file_name,subdest)
+        file.close()  
+
+
+
+def balance_train_and_test(source_train, source_test):
+    
+    for family in os.listdir(source_train):
+        
+        for classes in os.listdir(source_train+'/'+family):
+            
+            subfolder_train = source_train + '/' + family + '/' + classes
+            subfolder_test = source_test + '/' + family + '/' + classes
+            
+            if not os.path.exists(subfolder_test):
+                print(classes)
+                os.mkdir(subfolder_test)
+                pics_to_move = int(0.2*len(os.listdir(source_train + '/' + family + '/' + classes)))
+                images = os.listdir(subfolder_train)
+                
+                for i in range(pics_to_move):
+                    image = subfolder_train + '/' + images[i]
+                    shutil.move(image, subfolder_test)
+
+
+
+balance_train_and_test('sc5-tensorflow','sc5-test-tensorflow')
+split_into_subfolders('sc5-Test/ground_truth.txt')
+split_into_family_class('sc5/ground-truth-family.txt')
+split_test_into_family_class('sc5-Test/ground-truth-family.txt')
+
+
+
+def read_images(path, depth):
+    # initialize the data and labels
+    data = []
+    labels = []
+
+    # grab the image paths and randomly shuffle them
+    print("[INFO] loading images...")
+    imagePaths = sorted(list(paths.list_images(path)))
+    random.seed(42)
+    random.shuffle(imagePaths)
+
+    # loop over the input images
+    with tf.device("/GPU:0"):
+        for imagePath in imagePaths:
+            # load the image, pre-process it, and store it in the data list
+            image = cv2.imread(imagePath)
+            image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
+            image = img_to_array(image)
+            data.append(image)
+
+            # extract the class label from the image path and update the
+            # labels list
+            label = imagePath.split(os.path.sep)[-depth]
+            labels.append(label)
+    return data, labels
+
+
+
+a, b = read_images("sc5-tensorflow", 2)
+b.shape
+
